c26bfb1ff8e8701aa289a9466e0d662c39cce2d0
KuznecovAndrey7777777 <107943133+KuznecovAndrey7777777@users.noreply.github.com>
2022-10-30 20:53:40 +0200

    save301020222053

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,75 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const items = [];
+
+galleryItems.forEach((element) => {
+    const galleryItem = document.createElement("div");
+    galleryItem.className = "gallery__item";
+    const galleryLink = document.createElement("a");
+    galleryLink.className = "gallery__link";
+    galleryLink.href = element.original;
+    const galleryImage = document.createElement("img");
+    galleryImage.className = "gallery__image";
+    galleryImage.src = element.preview;
+    galleryImage.setAttribute("data-source", element.original);
+    galleryImage.alt = element.description;
+    galleryItem.append(galleryLink);
+    galleryLink.append(galleryImage);
+    items.push(galleryItem);
+});
+
+gallery.append(...items);
+gallery.addEventListener("click", (elem) => {
+    elem.preventDefault();
+    if (elem.target.nodeName !== "IMG") {
+        return;
+    }
+
+    const selectedImage = elem.target.getAttribute("data-source");
+    const instance = basicLightbox.create(
+        `<img src="${selectedImage}" width="800" height="600">`
+    );
+
+    instance.show();
+    gallery.addEventListener("keydown", (elem) => {
+        if (elem.key === "Escape") {
+            instance.close();
+        }
+    });
+});
+
+
+// Завдання 1 - галерея зображень
+// Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні. Подивися демо відео роботи галереї.
+
+// Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:
+
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+// Реалізація делегування на div.gallery і отримання url великого зображення.
+// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
+// Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
+// Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
+// Розмітка елемента галереї
+// Посилання на оригінальне зображення повинно зберігатися в data-атрибуті source на елементі <img>, і вказуватися в href посилання. Не додавай інші HTML теги або CSS класи, крім тих, що містяться в цьому шаблоні.
+
+// <div class="gallery__item">
+//   <a class="gallery__link" href="large-image.jpg">
+//     <img
+//       class="gallery__image"
+//       src="small-image.jpg"
+//       data-source="large-image.jpg"
+//       alt="Image description"
+//     />
+//   </a>
+// </div>
+
+// Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач буде перенаправлений на іншу сторінку. Заборони цю поведінку за замовчуванням.
+
+// Закриття з клавіатури
+// УВАГА
+// Наступний функціонал не обов'язковий для здавання завдання, але буде хорошою додатковою практикою.
+
+// Додай закриття модального вікна після натискання клавіші Escape. Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно. Бібліотека basicLightbox містить метод для програмного закриття модального вікна.
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const items = [];
+
+galleryItems.forEach((element) => {
+    const galleryLink = document.createElement("a");
+    galleryLink.className = "gallery__link";
+    galleryLink.href = element.original;
+    const galleryImage = document.createElement("img");
+    galleryImage.className = "gallery__image";
+    galleryImage.src = element.preview;
+    galleryImage.setAttribute("title", element.description);
+    galleryImage.alt = element.description;
+    galleryLink.append(galleryImage);
+    items.push(galleryLink);
+});
+gallery.append(...items);
+
+new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+});
+
+
+// Завдання 2 - бібліотека SimpleLightbox
+// Зроби таку саму галерею як в першому завданні, але використовуючи бібліотеку SimpleLightbox, яка візьме на себе обробку кліків по зображеннях, відкриття і закриття модального вікна, а також гортання зображень за допомогою клавіатури.
+
+// Необхідно трохи змінити розмітку картки галереї, використовуй цей шаблон.
+
+// <a class="gallery__item" href="large-image.jpg">
+//   <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+// </a>
+
+// Виконуй це завдання у файлах 02-lightbox.html і 02-lightbox.js. Розбий його на декілька підзавдань:
+
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї. Використовуй готовий код з першого завдання.
+// Підключення скрипту і стилів бібліотеки, використовуючи CDN сервіс cdnjs. Необхідно додати посилання на два файли: simple-lightbox.min.js і simple-lightbox.min.css.
+// Ініціалізація бібліотеки після створення і додання елементів галереї у div.gallery. Для цього ознайомся з документацією SimpleLightbox - насамперед секції «Usage» і «Markup».
+// Подивися в документації секцію «Options» і додай відображення підписів до зображень з атрибута alt. Нехай підпис буде знизу і з'являється через 250 мілісекунд після відкриття зображення.
